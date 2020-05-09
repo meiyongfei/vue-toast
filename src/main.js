898e0b23ad7cd59e59061415f92c51baddb17947
meiyongfei <meiyongfei@cmbi.com.hk>
2020-05-09 15:54:30 +0800

    修改版本号

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vueToast from '../packages/index'
+import vueToast from './packages/index'
 
 Vue.use(vueToast)
 
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // 修改 src 为 examples
   pages: {
     index: {
-      entry: 'examples/main.js',
+      entry: 'src/main.js',
       template: 'public/index.html',
       filename: 'index.html'
     }
